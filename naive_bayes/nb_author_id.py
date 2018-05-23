446d0c3d55d499e3821c3b9c6164c82a845b15df
kael <helong9291@sina.com>
2018-05-23 08:16:09 +0800

    0.完成迷你项目的初始化流程，1.完成朴素贝叶斯迷你项目，3.压缩包移除到外部

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,7 +26,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
 
+gnb = GaussianNB()
+
+t0 = time()
+gnb.fit(features_train, labels_train)
+print "Train time:", round(time()-t0, 3), "s"
+
+t0 = time()
+print 'Score:'+str(gnb.score(features_test, labels_test))
+print "Test time:", round(time()-t0, 3), "s"
 
 #########################################################
 
