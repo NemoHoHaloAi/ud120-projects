4de01613f57443cad762b6ab4f5d56d2c2c21293
kael <helong9291@sina.com>
2018-08-24 23:13:48 +0800

    完成聚类迷你项目

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python 
+# -*- coding: utf-8 -*-
 
 """ 
     Skeleton code for k-means clustering mini-project.
@@ -43,13 +44,38 @@ data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r")
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
+### exercised_stock_options 特征的最大最小值
+max_ = 0
+min_ = 99999999999999999
+for k in data_dict.keys():
+    v = data_dict[k]["exercised_stock_options"]
+    if not v == "NaN":
+        if v > max_:
+            max_ = v
+        elif v < min_:
+            min_ = v
+print 'max:'+str(max_)
+print 'min:'+str(min_)
+
+max_ = 0
+min_ = 99999999999999999
+for k in data_dict.keys():
+    v = data_dict[k]["salary"]
+    if not v == "NaN":
+        if v > max_:
+            max_ = v
+        elif v < min_:
+            min_ = v
+print 'max:'+str(max_)
+print 'min:'+str(min_)
 
 ### the input features we want to use 
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -58,14 +84,16 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, _ in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
-
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=2, random_state=0)
+pred = kmeans.fit_predict(finance_features)
 
 
 ### rename the "name" parameter when you change the number of features
